819b87d8a07c50647e5870adda19d77b293c5e69
James-Trema <andrdu29@live.fr>
2024-10-18 11:12:59 +0200

    up

--- app/index.php
+++ app/index.php
@@ -1,3 +1,15 @@
 <?php
-phpinfo();
+$servername = "db";
+$username = "root";
+$password = "example";
+$dbname = "mydb";
+
+// Créer la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
